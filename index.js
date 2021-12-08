b1913c1bff341e590d35bad2546e86b76e930fb7
Nick Romito <nick@cribl.io>
2021-12-08 17:46:46 -0600

    the most basic version of this util

---
+++ index.js
@@ -0,0 +1,22 @@
+const asciichart = require('asciichart');
+
+async function readStdin() {
+  const toPlot = [];
+  return await new Promise((resolve, reject) => {
+    process.stdin
+      .on('error', err => reject(err))
+      .on('data', chunk => toPlot.push(...chunk.toString().split('\n').map(v => +v).filter(v => !isNaN(v))))
+      .on('end', () => resolve(toPlot));
+  });
+}
+async function main() {
+  try {
+    const toPlot = await readStdin();
+    console.log(asciichart.plot(toPlot))
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+main();
